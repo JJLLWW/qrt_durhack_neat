cd5c3d2c635ca50da65c16de3b58653eb8a16545
Jack Wright <ugh@ugh.com>
2023-12-13 14:41:05 +0000

    skeleton of loading functionality complete

--- src/loglib/datamodel/log_file.py
+++ src/loglib/datamodel/log_file.py
@@ -6,18 +6,33 @@ LogEntry = namedtuple(
     field_names=["timestamp", "status", "message", "statistics"]
 )
 
+LogFileEntry = namedtuple(
+    typename="LogFileEntry",
+    field_names=["timestamp", "status", "message", "stats_id"]
+)
 
+
+# not thread safe
 class LogFile:
     def __init__(self):
-        self.log_entries = []
+        self.log_entries: list[LogFileEntry] = []
+        self.stat_frames: list[pd.DataFrame] = []
+        self.snapshot = None
 
     def add_entry(self, entry: LogEntry):
-        self.log_entries.append(entry)
+        file_entry = LogFileEntry(entry.timestamp, entry.status, entry.message, None)
+        if entry.statistics is not None:
+            stat_id = len(self.stat_frames)
+            file_entry.stats_id = stat_id
+            self.stat_frames.append(entry.statistics)
+        self.log_entries.append(file_entry)
 
     def add_entries(self, entries: list[LogEntry]):
         for entry in entries:
             self.add_entry(entry)
 
-    def get_file_snapshot(self) -> pd.DataFrame:
-        # this won't behave as expected.
-        return pd.DataFrame(data=self.log_entries)
+    # TODO: what about the stats auxiliary data frames?
+    def get_file_snapshot(self, regenerate=True) -> pd.DataFrame:
+        if self.snapshot is None or regenerate:
+            self.snapshot = pd.DataFrame(data=self.log_entries)
+        return self.snapshot
